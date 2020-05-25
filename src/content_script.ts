7799f896603f667dfd99ec46804adad9931844c2
EnixCoda <enixcoda@gmail.com>
2020-05-26 00:23:07 +0800

    chore: comment pjax by default

--- src/content_script.ts
+++ src/content_script.ts
@@ -1,5 +1,5 @@
-import { Pjax } from "pjax-api";
+// import { Pjax } from "pjax-api";
 
 console.log(`App is running!`);
 
-void new Pjax({}); // Above log won't work unless this line was commented out
+// void new Pjax({}); // Above log won't work unless this line was commented out
